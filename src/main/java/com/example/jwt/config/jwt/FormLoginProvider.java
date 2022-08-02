3cb245a7f07c5862195df58017a3fb635bfc124b
Seonghun Lee <aba7500@naver.com>
2022-08-02 23:46:44 +0900

    Provider 작동

--- src/main/java/com/example/jwt/config/jwt/FormLoginProvider.java
+++ src/main/java/com/example/jwt/config/jwt/FormLoginProvider.java
@@ -36,7 +36,7 @@ public class FormLoginProvider implements AuthenticationProvider {
     }
 
     @Override
-    public boolean supports(Class<?> authentication) {
-        return false;
+    public boolean supports(Class<?> authentication) {                    // token 타입에 따라서 언제 provider를 사용할지 조건을 지정할 수 있다.
+        return UsernamePasswordAuthenticationToken.class==authentication; // provider의 supports 값이 false를 리턴하면, provider의 authenticate 메소드가 호출되지 않는다.
     }
 }
